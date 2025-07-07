9f2b01606bf044a61f095227fe24a12b8d3d1830
maachamundadigitalsolutions <maachamundadigitalsolutions@gmail.com>
2025-07-07 23:51:16 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
-// api/index.js
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Express deployed on Vercel!');
+  res.json({
+    success: true,
+    message: 'Welcome to the Express API on Vercel!',
+    data: {
+      name: 'Vishnu Solanki',
+      role: 'MERN Stack Developer',
+      company: 'Maa Chamunda Digital Solutions',
+      website: 'https://maachamundadigitalsolutions.com'
+    }
+  });
 });
 
-// Export as Vercel serverless function
 module.exports = app;
